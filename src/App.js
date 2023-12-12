130125dbe5ba1703a275ca4209fb7222cd74e89a
axel-ac <abdullahceylande@gmail.com>
2023-12-12 19:45:16 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App flex'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
